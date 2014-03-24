546c5223c217ad6f0e3bd2cc6cdda2c3bf4f82dd
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-24 10:29:09 -0400

    ran rails g devise and created store model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SizeCloset::Application.routes.draw do
+
+  root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
