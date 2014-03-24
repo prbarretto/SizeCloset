e157badf95275bd502e58f6c3919819477d947b8
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-24 18:51:15 -0400

    added views

--- app/controllers/closets_controller.rb
+++ app/controllers/closets_controller.rb
@@ -1,5 +1,5 @@
 class ClosetsController < ApplicationController
-  before_action :set_closet_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_closet, only: [:show, :edit, :update, :destroy]
 
   def index
     @closets = Closet.all
@@ -16,42 +16,42 @@ class ClosetsController < ApplicationController
   end
 
   def create
-    @closet_item = Closet.new(closet_params)
+    @closet = Closet.new(closet_params)
 
     respond_to do |format|
-      if @closet_item.save
-        format.html { redirect_to @closet_item, notice: 'An item was added to your closet.' }
-        format.json { render action: 'show', status: :created, location: @closet_item }
+      if @closet.save
+        format.html { redirect_to @closet, notice: 'An item was added to your closet.' }
+        format.json { render action: 'show', status: :created, location: @closet }
       else
         format.html { render action: 'new' }
-        format.json { render json: @closet_item.errors, status: :unprocessable_entity }
+        format.json { render json: @closet.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
     respond_to do |format|
-      if @closet_item.update(closet_item_params)
-        format.html { redirect_to @closet_item, notice: 'Your item was successfully updated.' }
+      if @closet.update(closet_params)
+        format.html { redirect_to @closet, notice: 'Your item was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @closet_item.errors, status: :unprocessable_entity }
+        format.json { render json: @closet.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @closet_item.destroy
+    @closet.destroy
     respond_to do |format|
-      format.html { redirect_to closet_item_url }
+      format.html { redirect_to closet_url }
       format.json { head :no_content }
     end
   end
 
   private
-  def set_closet_item
-      @closet_item = Closet.find(params[:id])
+  def set_closet
+      @closet = Closet.find(params[:id])
     end
 
   def closet_params
