32fe805ca7d173bbaf19b43d8995cdb9d2f3d037
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-24 09:26:34 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SizeCloset::Application.config.secret_key_base = 'bf3a0501779b104bd1ca7a35b845c8aeddca1cd9a52b073224a0ef57bbb908b573bbdef44d38f16e79c0dd97db0068c29f5d124ea1279422132f9f79d9d73ec7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SizeCloset::Application.config.session_store :cookie_store, key: '_SizeCloset_session'
